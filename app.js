2d7c824963ef3145af37387a8b8a3b0d783d87b8
Asharib Ali <96954816+Ashrib@users.noreply.github.com>
2022-06-29 19:05:37 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,21 +6,26 @@ function addword() {
     var consonants = [];
     var boolean = false;
     
-    for(i = 0; i < userValue.length; i++){
-        if(userValue[i] === "a" || userValue[i] === "e" || userValue[i] === "i" || userValue[i] === "o" || userValue[i] === "u" ){   
-            boolean = true;break;
-        }
-    }
-    if(boolean === true){
-        vowels.push(userInput);
+    if(userValue === ""){
+        document.getElementById("alert").innerText = "You didn't input !";
     }
     else{
-        consonants.push(userInput);
-    }
-    for(j = 0; j < vowels.length; j++){
-        document.getElementById("vowelwords").innerHTML += "<li>"+ vowels[j] + "</li>";
-    }
-    for(k = 0; k < consonants.length; k++){
-        document.getElementById("consonantwords").innerHTML += "<li>"+ consonants[k] + "</li>";
-    }
-}
+        for(i = 0; i < userValue.length; i++){
+            if(userValue[i] === "a" || userValue[i] === "e" || userValue[i] === "i" || userValue[i] === "o" || userValue[i] === "u" ){   
+                boolean = true;break;
+            };
+        };
+        if(boolean === true){
+            vowels.push(userInput);
+        }
+        else{
+            consonants.push(userInput);
+        };
+        for(j = 0; j < vowels.length; j++){
+            document.getElementById("vowelwords").innerHTML += "<li>"+ vowels[j] + "</li>";
+        };
+        for(k = 0; k < consonants.length; k++){
+            document.getElementById("consonantwords").innerHTML += "<li>"+ consonants[k] + "</li>";
+        };
+    };
+};
